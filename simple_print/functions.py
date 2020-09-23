8bd23dcc8dbbb7386f346395cd8a2b77b0b80f5d
Andrey Sobolev <email.asobolev@gmail.com>
2020-09-23 06:45:22 +0300

    Sobolev:LittleFix

--- simple_print/functions.py
+++ simple_print/functions.py
@@ -27,10 +27,10 @@ def sprint(*args):
 
         if variables:              
             print_array = []
-            variables_list_len = len(variables) - 1
+            len_variables = len(variables) - 1
             for i, variable in enumerate(variables):
 
-                if variables_list_len == i and variables_list_len > 0 and variable.strip('"').strip("'") in ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']:
+                if len_variables == i and len_variables > 0 and variable.strip('"').strip("'") in ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']:
                     color = variables[-1].strip('"').strip("'")
                     continue
 
@@ -40,12 +40,12 @@ def sprint(*args):
                     print_array.append(f'{variable}={args[i]}')
 
             if print_array:
-                print_string = '; '.join(print_array)  
+                print_string = ' :: '.join(print_array)  
 
     if print_string:        
-        cprint(f'{print_string} :: line {lineno}', color, attrs=['bold'])
+        cprint(f'{print_string} :: line {lineno}', color, 'on_cyan', attrs=['bold'])
     else:
-        print(f'{args} :: line {lineno}', color, attrs=['bold'])
+        cprint(f'{code} :: line {lineno}', color, 'on_cyan', attrs=['bold'])
 
 
 def sprint_f(*args):
@@ -69,10 +69,10 @@ def sprint_f(*args):
 
         if variables:              
             print_array = []
-            variables_list_len = len(variables) - 1
+            len_variables = len(variables) - 1
             for i, variable in enumerate(variables):
 
-                if variables_list_len == i and variables_list_len > 0 and variable.strip('"').strip("'") in ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']:
+                if len_variables == i and len_variables > 0 and variable.strip('"').strip("'") in ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']:
                     color = variables[-1].strip('"').strip("'")
                     continue
 
@@ -82,9 +82,9 @@ def sprint_f(*args):
                     print_array.append(f'{variable}={args[i]}')
 
             if print_array:
-                print_string = '; '.join(print_array)  
+                print_string = ' :: '.join(print_array)  
 
     if print_string:        
         cprint(f'{print_string} :: line {lineno} :: {filename}', color, attrs=['bold'])
     else:
-        cprint(f'{args} :: line {lineno} :: {filename}', color, attrs=['bold'])
+        cprint(f'{code} :: line {lineno} :: {filename}', color, attrs=['bold'])
