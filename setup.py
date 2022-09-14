4d7796520446e3e9231f028c150bc2d79b03f9a1
Andrey Sobolev <email.asobolev@gmail.com>
2022-09-14 08:45:44 +0000

    Sobolex:FixSetup

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='simple-print',
     version=__version__,
-    install_requires=['termcolor==1.1.0', 'executing==0.8.1', 'asttokens==2.0.5'],
+    install_requires=['termcolor==1.1.0', 'executing==0.8.1', 'asttokens==2.0.5', 'typing-extensions>=4.1.0'],
     extras_require={ 
-        'broker': ['pika>=1.3.0', 'pydantic>=1.10.2', 'typing-extensions>=4.1.0', 'orjson>=3.8.0'], 
+        'broker': ['pika>=1.3.0', 'pydantic>=1.10.2', 'orjson>=3.8.0'], 
     },
     author='Sobolev Andrey',
     author_email='email.asobolev@gmail.com',
